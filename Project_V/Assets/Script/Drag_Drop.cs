c83340681e89aaecf1171d958698decfc03b216f
Hysteria0K <tksthqo123@gmail.com>
2024-07-02 22:49:38 +0900

    Dragdrop Updated

--- Project_V/Assets/Script/Drag_Drop.cs
+++ Project_V/Assets/Script/Drag_Drop.cs
@@ -6,9 +6,15 @@ using UnityEngine.UI;
 
 public class Drag_Drop : MonoBehaviour, IDragHandler, IPointerDownHandler
 {
+    private Vector2 Vect2;
+    private Vector3 Saved_Position;
+
+    private RectTransform ThisRect;
+
     // Start is called before the first frame update
     void Start()
     {
+        ThisRect= GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
@@ -19,11 +25,13 @@ public class Drag_Drop : MonoBehaviour, IDragHandler, IPointerDownHandler
 
     void IDragHandler.OnDrag(UnityEngine.EventSystems.PointerEventData eventData)
     {
-        transform.position = eventData.position;
+        transform.position = eventData.position - (Vect2 - new Vector2(Saved_Position.x, Saved_Position.y));
     }
 
     void IPointerDownHandler.OnPointerDown(UnityEngine.EventSystems.PointerEventData eventData)
     {
         transform.SetAsLastSibling();
+        Vect2 = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Saved_Position = ThisRect.position;
     }
 }
