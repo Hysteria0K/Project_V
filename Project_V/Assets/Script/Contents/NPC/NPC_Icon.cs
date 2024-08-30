f12f081dcb10853d7d3fa2c80b583baa7ea39fff
Hysteria0K <tksthqo123@gmail.com>
2024-08-30 16:35:09 +0900

    캐릭터 변경 Fade 없이 적용

--- Project_V/Assets/Script/Contents/NPC/NPC_Icon.cs
+++ Project_V/Assets/Script/Contents/NPC/NPC_Icon.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class NPC_Icon : MonoBehaviour
+public class NPC_Icon : MonoBehaviour, IPointerDownHandler
 {
     public NPC_Select Npc_Select;
     public Sprite_Reader SpriteReader;
     public JsonReader JsonReader;
 
     [Header("Character")]
-    [SerializeField] private string Character_Name;
+    public string Character_Name;
 
     [Header("나중에세이브데이터로뺄거")]
     public bool Available;
@@ -36,9 +37,15 @@ public class NPC_Icon : MonoBehaviour
         // 나중에 언제 Unload할지 생각
     }
 
-    // Update is called once per frame
-    void Update()
+    void IPointerDownHandler.OnPointerDown(UnityEngine.EventSystems.PointerEventData eventData)
     {
-        
+        if (Npc_Select.Selected_Character != Character_Name)
+        {
+            Npc_Select.Temp_Char = Npc_Select.Selected_Character;
+            Npc_Select.Selected_Character = Character_Name;
+            Npc_Select.Change_Standing_Image();
+            SpriteReader.LoadSprite(this.GetComponent<Image>(), JsonReader.Character_Dictionary[Character_Name].Face_Sprite_Mono);
+            Npc_Select.Change_Icon_Image();
+        }
     }
 }
--- Project_V/Assets/Script/Contents/NPC/NPC_Select.cs
+++ Project_V/Assets/Script/Contents/NPC/NPC_Select.cs
@@ -9,15 +9,18 @@ public class NPC_Select : MonoBehaviour
     public Sprite_Reader SpriteReader;
     public GameObject Select_Closed;
     public GameObject Select_Open;
+    public Image Character_Sprite;
+    public GameObject Char_Icon;
+
     public bool Is_Open;
     public string Selected_Character;
-    public Image Character_Sprite;
+    public string Temp_Char;
 
     // Start is called before the first frame update
     void Start()
     {
         Is_Open = false;
-        SpriteReader.LoadSprite(Character_Sprite, JsonReader.Character_Dictionary[Selected_Character].Standing_Sprite);
+        Change_Standing_Image();
     }
 
     // Update is called once per frame
@@ -41,5 +44,23 @@ public class NPC_Select : MonoBehaviour
             Select_Open.SetActive(false);
             Is_Open = false;
         }
+    }
+
+    public void Change_Standing_Image()
+    {
+        SpriteReader.LoadSprite(Character_Sprite, JsonReader.Character_Dictionary[Selected_Character].Standing_Sprite);
+    }
+
+    public void Change_Icon_Image()
+    {
+        for (int i = 0; i < Char_Icon.transform.childCount; i++)
+        {
+            if (Char_Icon.transform.GetChild(i).GetComponent<NPC_Icon>().Character_Name == Temp_Char)
+            {
+                SpriteReader.LoadSprite(Char_Icon.transform.GetChild(i).GetComponent<Image>(), JsonReader.Character_Dictionary[Temp_Char].Face_Sprite);
+                break;
+            }
+        }
+        Temp_Char = "";
     }
 }
