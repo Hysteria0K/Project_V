5ef193b85bf704f38c0ff91c51fb7a7bfdc13a14
Hysteria0K <tksthqo123@gmail.com>
2024-08-30 16:12:14 +0900

    스프라이트 불러오기 추가

--- Project_V/Assets/Script/Contents/NPC/NPC_Select.cs
+++ Project_V/Assets/Script/Contents/NPC/NPC_Select.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPC_Select : MonoBehaviour
 {
-    public JsonReader Jsonreader;
+    public JsonReader JsonReader;
+    public Sprite_Reader SpriteReader;
     public GameObject Select_Closed;
     public GameObject Select_Open;
     public bool Is_Open;
     public string Selected_Character;
+    public Image Character_Sprite;
 
     // Start is called before the first frame update
     void Start()
     {
         Is_Open = false;
+        SpriteReader.LoadSprite(Character_Sprite, JsonReader.Character_Dictionary[Selected_Character].Standing_Sprite);
     }
 
     // Update is called once per frame
