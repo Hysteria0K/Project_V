ba07ac311f4b7b6f43c701fed5599cfd8b676367
Hysteria0K <tksthqo123@gmail.com>
2024-10-06 22:57:47 +0900

    WriteLetter Day -> Id로 변경

--- Project_V/Assets/Script/Contents/WriteLetter/WriteLetter_JsonReader.cs
+++ Project_V/Assets/Script/Contents/WriteLetter/WriteLetter_JsonReader.cs
@@ -14,7 +14,7 @@ public class WriteLetter_JsonReader : MonoBehaviour
     [System.Serializable]
     public class WriteLetter_Attributes
     {
-        public int Day;
+        public string Id;
         public int Turn;
         public int Order;
         public string Text;
@@ -34,17 +34,17 @@ public class WriteLetter_JsonReader : MonoBehaviour
     #endregion Parse
 
     public WriteLetter_Parse WriteLetter;
-    public Dictionary<int, Dictionary<int, Dictionary<int, WriteLetter_Attributes>>> WriteLetter_Dictionary;
+    public Dictionary<string, Dictionary<int, Dictionary<int, WriteLetter_Attributes>>> WriteLetter_Dictionary;
 
     private void Awake()
     {
         WriteLetter = JsonUtility.FromJson<WriteLetter_Parse>(ReadJson("writeletter"));
 
-        WriteLetter_Dictionary = new Dictionary<int, Dictionary<int, Dictionary<int, WriteLetter_Attributes>>>();
+        WriteLetter_Dictionary = new Dictionary<string, Dictionary<int, Dictionary<int, WriteLetter_Attributes>>>();
         WriteLetter_To_Dictionary();
 
         Debug.Log("JsonParse Complete");
-        //Debug.Log(WriteLetter_Dictionary[1][3][1].Tag1); //WriteLetter_Dictionary[Day][Turn][Order].<Attributes>
+        //Debug.Log(WriteLetter_Dictionary[1][3][1].Tag1); //WriteLetter_Dictionary[Id][Turn][Order].<Attributes>
     }
 
     void Start()
@@ -69,35 +69,35 @@ public class WriteLetter_JsonReader : MonoBehaviour
     #region 배열, 자료형으로 변환
     private void WriteLetter_To_Dictionary()
     {
-        Dictionary<int, Dictionary<int, WriteLetter_Attributes>> Day_Dictionary = new Dictionary<int, Dictionary<int, WriteLetter_Attributes>>();
+        Dictionary<int, Dictionary<int, WriteLetter_Attributes>> Id_Dictionary = new Dictionary<int, Dictionary<int, WriteLetter_Attributes>>();
         Dictionary<int, WriteLetter_Attributes> Turn_Dictionary = new Dictionary<int, WriteLetter_Attributes>();
 
-        int Saved_Day = WriteLetter.writeletter[0].Day;
+        string Saved_Id = WriteLetter.writeletter[0].Id;
         int Saved_Turn = WriteLetter.writeletter[0].Turn;
 
         for (int i = 0; i < WriteLetter.writeletter.Length; i++)
         {
-            if (Saved_Day != WriteLetter.writeletter[i].Day)
+            if (Saved_Id != WriteLetter.writeletter[i].Id)
             {
-                Day_Dictionary.Add(Saved_Turn, Turn_Dictionary);
-                WriteLetter_Dictionary.Add(Saved_Day, Day_Dictionary);
-                Day_Dictionary = new Dictionary<int, Dictionary<int, WriteLetter_Attributes>>();
+                Id_Dictionary.Add(Saved_Turn, Turn_Dictionary);
+                WriteLetter_Dictionary.Add(Saved_Id, Id_Dictionary);
+                Id_Dictionary = new Dictionary<int, Dictionary<int, WriteLetter_Attributes>>();
                 Turn_Dictionary = new Dictionary<int, WriteLetter_Attributes>();
-                Saved_Day = WriteLetter.writeletter[i].Day;
+                Saved_Id = WriteLetter.writeletter[i].Id;
                 Saved_Turn = WriteLetter.writeletter[i].Turn;
             }
 
             if (Saved_Turn != WriteLetter.writeletter[i].Turn)
             {
-                Day_Dictionary.Add(Saved_Turn, Turn_Dictionary);
+                Id_Dictionary.Add(Saved_Turn, Turn_Dictionary);
                 Turn_Dictionary = new Dictionary<int, WriteLetter_Attributes>();
                 Saved_Turn = WriteLetter.writeletter[i].Turn;
             }
 
             Turn_Dictionary.Add(WriteLetter.writeletter[i].Order, WriteLetter.writeletter[i]);
         }
-        Day_Dictionary.Add(Saved_Turn, Turn_Dictionary);
-        WriteLetter_Dictionary.Add(Saved_Day, Day_Dictionary);
+        Id_Dictionary.Add(Saved_Turn, Turn_Dictionary);
+        WriteLetter_Dictionary.Add(Saved_Id, Id_Dictionary);
     }
     #endregion 배열, 자료형으로 변환
 }
--- Project_V/Assets/Script/Contents/WriteLetter/WriteLetter_Manager.cs
+++ Project_V/Assets/Script/Contents/WriteLetter/WriteLetter_Manager.cs
@@ -7,7 +7,7 @@ using Unity.VisualScripting;
 public class WriteLetter_Manager : MonoBehaviour
 {
     [Header("Control")]
-    public int Day;
+    public string Id;
     [Space(15f)]
     public int Turn;
 
@@ -33,7 +33,7 @@ public class WriteLetter_Manager : MonoBehaviour
 
     void Awake()
     {
-        // Day 불러오기 - 나중에 연결할 때 만들어야함 프로토타입에서는 인스펙터에서 제어함.
+        // Id 불러오기 - 나중에 연결할 때 만들어야함 프로토타입에서는 인스펙터에서 제어함.
         Turn = 1;
     }
 
@@ -49,13 +49,13 @@ public class WriteLetter_Manager : MonoBehaviour
 
     public void Spawn_WriteText()
     {
-        for(int i =1; i<= WriteLetter_JsonReader.WriteLetter_Dictionary[Day][Turn].Count; i++ )
+        for(int i =1; i<= WriteLetter_JsonReader.WriteLetter_Dictionary[Id][Turn].Count; i++ )
         {
-            WriteLetter_Text.GetComponent<WriteLetter_Text>().MainText = WriteLetter_JsonReader.WriteLetter_Dictionary[Day][Turn][i].Text;
-            WriteLetter_Text.GetComponent<WriteLetter_Text>().Tag1 = WriteLetter_JsonReader.WriteLetter_Dictionary[Day][Turn][i].Tag1;
-            WriteLetter_Text.GetComponent<WriteLetter_Text>().Tag2 = WriteLetter_JsonReader.WriteLetter_Dictionary[Day][Turn][i].Tag2;
-            WriteLetter_Text.GetComponent<WriteLetter_Text>().Tag3 = WriteLetter_JsonReader.WriteLetter_Dictionary[Day][Turn][i].Tag3;
-            WriteLetter_Text.GetComponent<WriteLetter_Text>().MasterPiece = WriteLetter_JsonReader.WriteLetter_Dictionary[Day][Turn][i].Masterpiece;
+            WriteLetter_Text.GetComponent<WriteLetter_Text>().MainText = WriteLetter_JsonReader.WriteLetter_Dictionary[Id][Turn][i].Text;
+            WriteLetter_Text.GetComponent<WriteLetter_Text>().Tag1 = WriteLetter_JsonReader.WriteLetter_Dictionary[Id][Turn][i].Tag1;
+            WriteLetter_Text.GetComponent<WriteLetter_Text>().Tag2 = WriteLetter_JsonReader.WriteLetter_Dictionary[Id][Turn][i].Tag2;
+            WriteLetter_Text.GetComponent<WriteLetter_Text>().Tag3 = WriteLetter_JsonReader.WriteLetter_Dictionary[Id][Turn][i].Tag3;
+            WriteLetter_Text.GetComponent<WriteLetter_Text>().MasterPiece = WriteLetter_JsonReader.WriteLetter_Dictionary[Id][Turn][i].Masterpiece;
 
             Instantiate(WriteLetter_Text, TextPositionSet.GetChild(i - 1).transform.position, new Quaternion(0, 0, 0, 0), Select_Text);
         }
